e55fe9f4ab866d7947526d6f399f16087c385682
Hito 0/1 <hito94101@gmail.com>
2025-03-27 01:55:33 +0100

    pontocardeal.c

--- pontocardeal.c
+++ pontocardeal.c
@@ -1,1 +1,31 @@
+void cardealpoint(void){
+    puts("\n");
+	system("color c");
+	puts("\t\t\t  N");
+	puts("\t\t\t  *");
+	puts("\t\t\t  *");
+	puts("          No\t\t  *\t\tNe");
+	puts("\t\t\t  *");
+	puts("\t\t\t  *");
+	puts("\t\t\t  *");
+	puts("\t\t\t  *");
+	puts("\t\t\t  *");
+	system("color 5");
+	printf("\tO *************** 0 **************** E\n");
+	system("color 6");
+	puts("\t\t\t  *");
+	puts("\t\t\t  *");
+	puts("\t\t\t  *");
+	puts("\t\t\t  *");
+	puts("          So\t\t  *\t\tSe");
+	puts("\t\t\t  *");
+	puts("\t\t\t  *");
+	puts("\t\t\t  *");
+	puts("\t\t\t  S");
+	system("color 0");
+}
 
+int main(void){
+  cardealpoint();
+  return 0;
+}
